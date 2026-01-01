715115806b221aa987f62e9deee6db21be9a8098
DuckTri <huutri812007@gmail.com>
2026-01-01 11:53:45 +0700

    Week1_Ex1

---
+++ Week_1/Bai001.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void input(double &n, double &m){
+  cin >> n >> m;
+}
+
+double sum(double n, double m){
+  return n + m;
+}
+
+
+
+int main(){
+  double n;
+}
